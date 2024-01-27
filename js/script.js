6a6a3deceafb6ab2e559e7437e51d72b6a9808e2
marceloaaguilar <marcelodalmeida42@gmail.com>
2024-01-27 09:20:56 -0300

    New Features

--- js/script.js
+++ js/script.js
@@ -22,6 +22,15 @@ pagination: {
     el: '.swiper-pagination',
 },
 
+// Navigation arrows
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false
+},
 });
 
 
